29a88459e6eec3a78b7f55d87a59aa7552cbad80
mingmingjiang1 <157197768@qq.com>
2023-07-29 16:13:29 +0000

    feat: init

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -5,15 +5,13 @@ using namespace std;
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
   // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  return Wrap32(static_cast<uint32_t>(n & 0x00000000ffffffff) + zero_point.raw_value_);
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+   int32_t offset = this->raw_value_ - wrap(checkpoint, zero_point).raw_value_;
+   int64_t res = checkpoint + offset;
+   return res >= 0 ? res : res + (1ul << 32);
 }
